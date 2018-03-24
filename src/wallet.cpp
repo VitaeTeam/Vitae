9b65d46e3a7fd003d4c623bc83bb25ca9e92e726
warrows <warrows@gamer666.fr>
2018-03-24 17:18:47 +0100

    [Wallet] Stop removing spent zPIV hashes from map

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2157,7 +2157,7 @@ bool CWallet::MintableCoins()
     }
 
     for (auto it : mapSerialHashes) {
-        if (it.second.nVersion < CZerocoinMint::STAKABLE_VERSION)
+        if (it.second.nVersion < CZerocoinMint::STAKABLE_VERSION || it.second.isUsed)
             continue;
         if (it.second.nHeight < chainActive.Height() - Params().Zerocoin_RequiredStakeDepth()) {
             return true;
