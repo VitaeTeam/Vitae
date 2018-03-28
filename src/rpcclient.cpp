0b017c12dec5da1b2e09a23d43e06b31bca84e0a
presstab <presstab1337@gmail.com>
2018-03-28 10:37:48 -0600

    Check for existence of zpivTracker in ListMintedCoins().

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -138,6 +138,8 @@ static const CRPCConvertParam vRPCConvertParams[] =
         {"getspentzerocoinamount", 1},
         {"generatemintlist", 0},
         {"generatemintlist", 1},
+        {"searchdzpiv", 0},
+        {"searchdzpiv", 1},
         {"getfeeinfo", 0}
     };
 
