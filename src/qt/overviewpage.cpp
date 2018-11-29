4cb7cab27cea3dcc3be30b7fcbbae1f23f709775
Mrs-X <Mrs-X@users.noreply.github.com>
2018-11-29 21:40:55 +0100

    Merge #747: [GUI] Hide orphans - contextMenu action
    b3ff4a1 [GUI] Add action to hide orphans in overview and txlist (random-zebra)
    
    Tree-SHA512: 2b1a6fa71b98881448f4f0148b3a4b02e87ced4ff9212b98050fa717949ec85e4a523533896d9e20d8fefa65d2f2dcf3f0d4152f36a0dcb7def4a8ff4efa64e6

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -412,5 +412,6 @@ void OverviewPage::showOutOfSyncWarning(bool fShow)
 
 void OverviewPage::hideOrphans(bool fHide)
 {
-    filter->setHideOrphans(fHide);
+    if (filter)
+        filter->setHideOrphans(fHide);
 }
