133c9e0ab018f11215b2f77e897eb612d3860586
Mrs-X <Mrs-X@users.noreply.github.com>
2018-12-01 21:35:25 +0100

    Merge #770: Do not record zerocoin tx's in ConnectBlock() if it is fJustCheck
    f260f5b Do not record zerocoin tx's in ConnectBlock() if it is fJustCheck (presstab)
    
    Tree-SHA512: fb5cf7294a4614fc10b862821d73ab5e47f78263e55837f553b146345fcd4043d62cd3026535ca1ca07f4070ff61deb582907848dc0b447c1803de430b79b5f3
    
    # Conflicts:
    #	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2986,7 +2986,7 @@ bool ReindexAccumulators(list<uint256>& listMissingCheckpoints, string& strError
     return true;
 }
 
-bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex)
+bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck)
 {
     std::list<CZerocoinMint> listMints;
     bool fFilterInvalid = pindex->nHeight >= Params().Zerocoin_Block_RecalculateAccumulators();
@@ -3011,7 +3011,7 @@ bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex)
             pindex->mapZerocoinSupply.at(denom)++;
 
             //Remove any of our own mints from the mintpool
-            if (pwalletMain) {
+            if (!fJustCheck && pwalletMain) {
                 if (pwalletMain->IsMyMint(m.GetValue())) {
                     pwalletMain->UpdateMint(m.GetValue(), pindex->nHeight, m.GetTxHash(), m.GetDenomination());
 
@@ -3278,7 +3278,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
 
     //Track zVITAE money supply in the block index
-    if (!UpdateZVITAESupply(block, pindex))
+    if (!UpdateZVITAESupply(block, pindex, fJustCheck))
         return state.DoS(100, error("%s: Failed to calculate new zVITAE supply for block=%s height=%d", __func__,
                                     block.GetHash().GetHex(), pindex->nHeight), REJECT_INVALID);
 
@@ -3344,11 +3344,13 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     //Record zVITAE serials
     if (pwalletMain) {
         std::set<uint256> setAddedTx;
-        for (const std::pair<libzerocoin::CoinSpend, uint256>& pSpend : vSpends) {
+        for (std::pair<CoinSpend, uint256> pSpend : vSpends) {
             // Send signal to wallet if this is ours
             if (pwalletMain->IsMyZerocoinSpend(pSpend.first.getCoinSerialNumber())) {
-                LogPrintf("%s: %s detected zerocoinspend in transaction %s \n", __func__, pSpend.first.getCoinSerialNumber().GetHex(), pSpend.second.GetHex());
-                pwalletMain->NotifyZerocoinChanged(pwalletMain, pSpend.first.getCoinSerialNumber().GetHex(), "Used", CT_UPDATED);
+                LogPrintf("%s: %s detected zerocoinspend in transaction %s \n", __func__,
+                          pSpend.first.getCoinSerialNumber().GetHex(), pSpend.second.GetHex());
+                pwalletMain->NotifyZerocoinChanged(pwalletMain, pSpend.first.getCoinSerialNumber().GetHex(), "Used",
+                                                   CT_UPDATED);
 
                 //Don't add the same tx multiple times
                 if (setAddedTx.count(pSpend.second))
