21cd584234d22b5c95e02904017b256b60ae951b
tohsnoom <moonshot@phore.io>
2018-12-11 22:53:04 -0600

    Updated checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -69,11 +69,12 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (588223, uint256("5374231f986b41d3745a1ee5d45b7decc65097712cb7d34429f0e1faecb8e5f4"))
     (598090, uint256("b753f2a3ba8837ed9efcc9f877f33266b342006c0c8818071b48726e58d2052c"))
     (635700, uint256("a90a034b2999c6a3deb69a74d60d01cf5cb78bae8cd8e29ddb4620baf6d67cce"));
+    (644300, uint256("c1330d826fb13968862bea2a84bd7b19f8a8cc93a83df1b2fe92bbea7050fafa"));
 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1544065114, // * UNIX timestamp of last checkpoint block
-    1334352,     // * total number of transactions between genesis and last checkpoint
+    1544586370, // * UNIX timestamp of last checkpoint block
+    1351856,     // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     1500        // * estimated number of transactions per day after checkpoint
 };
