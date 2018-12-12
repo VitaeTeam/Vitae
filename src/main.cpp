79f0548bfbec943f89c24e70cab5b5aa6886cbb8
tohsnoom <moonshot@phore.io>
2018-12-11 23:05:06 -0600

    Incremented protocol version and alternated protocol enforcement spork

--- src/main.cpp
+++ src/main.cpp
@@ -6647,7 +6647,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 //       it was the one which was commented out
 int ActiveProtocol()
 {
-    if (IsSporkActive(SPORK_18_NEW_PROTOCOL_ENFORCEMENT_3))
+    if (IsSporkActive(SPORK_14_NEW_PROTOCOL_ENFORCEMENT))
         return MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT;
     return MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT;
 }
