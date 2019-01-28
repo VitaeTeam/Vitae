05b2699c2dc76fa4244fd2d610db6fa1884be4a0
Squid <squid@sqdmc.net>
2019-01-28 05:19:21 -0500

    dont check mnsync for now..

--- src/miner.cpp
+++ src/miner.cpp
@@ -557,7 +557,7 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
             }
 
             while (vNodes.empty() || pwallet->IsLocked() || !fMintableCoins || (pwallet->GetBalance() > 0 && 
-                   nReserveBalance >= pwallet->GetBalance()) || !masternodeSync.IsSynced() || !fundamentalnodeSync.IsSynced()) {
+                   nReserveBalance >= pwallet->GetBalance()) /*|| !masternodeSync.IsSynced()*/ || !fundamentalnodeSync.IsSynced()) {
                 nLastCoinStakeSearchInterval = 0;
                 // Do a separate 1 minute check here to ensure fMintableCoins is updated
                 if (!fMintableCoins) {
