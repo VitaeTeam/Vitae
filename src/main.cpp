a13d52da116fa87ab85fa71184537b22d4b5b197
Julian Meyer <julianmeyer2000@gmail.com>
2019-01-29 15:12:50 -0800

    fix bug in fork checking algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -4598,7 +4598,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
             CBlockIndex *last = pindexPrev;
 
             // while that block is not on the main chain
-            while (!chainActive.Contains(last) && pindexPrev != NULL) {
+            while (!chainActive.Contains(last) && last != NULL) {
                 CBlock bl;
                 ReadBlockFromDisk(bl, last);
                 // loop through every spent input from said block
@@ -4616,7 +4616,7 @@ bool AcceptBlock(CBlock& block, CValidationState& state, CBlockIndex** ppindex, 
                 }
 
                 // go to the parent block
-                last = pindexPrev->pprev;
+                last = last->pprev;
             }
         }
     }
