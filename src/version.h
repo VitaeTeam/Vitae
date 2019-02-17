f49c7f4bcfab54eb67a8cce307a3411c818bd425
tohsnoom <moonshot@phore.io>
2019-02-17 15:52:26 -0600

    Raised minimum protocol version to 70007 (v1.6.0)

--- src/version.h
+++ src/version.h
@@ -21,7 +21,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70002;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70007;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70007;
 
 static const int MIN_PEER_VERSION_FIXED_SIGTIME = 70004;
