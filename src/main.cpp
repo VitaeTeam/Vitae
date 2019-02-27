100e46030654cb7a797c61a9343b508e925fa02a
Mrs-X <Mrs-X@users.noreply.github.com>
2019-02-27 21:51:22 +0100

    Merge #817: [Wallet] Fix segfault with runtime -disablewallet
    bae276a Don't bother checking obfuscation messages (Fuzzbawls)
    837f25f [Wallet] Fix segfault with runtime -disablewallet (Fuzzbawls)
    
    Tree-SHA512: a8dd502897cdc82f206a1805f7b1ae51960344c3fa32640c9dc6c271768e7db718bc2f57034e8fabfd1b402e589e531419d1e8af8a25eee71d89a6b79ce3a679
    
    # Conflicts:
    #	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7018,7 +7018,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
         }
     } else {
         //probably one the extensions
-        obfuScationPool.ProcessMessageObfuscation(pfrom, strCommand, vRecv);
+        //obfuScationPool.ProcessMessageObfuscation(pfrom, strCommand, vRecv);
         mnodeman.ProcessMessage(pfrom, strCommand, vRecv);
         budget.ProcessMessage(pfrom, strCommand, vRecv);
         fundamentalnodePayments.ProcessMessageFundamentalnodePayments(pfrom, strCommand, vRecv);
