247c2d78e8b42a89ff27a7affae8176e26ae87a0
furszy <5377650+furszy@users.noreply.github.com>
2019-04-02 18:36:42 +0200

    more changes on sendwidget
    # Conflicts:
    #	src/qt/vitae/send.cpp
    #	src/qt/vitae/send.h

--- src/qt/vitae/VITAEGUI.cpp
+++ src/qt/vitae/VITAEGUI.cpp
@@ -467,6 +467,9 @@ bool VITAEGUI::addWallet(const QString& name, WalletModel* walletModel)
     receiveWidget->setWalletModel(walletModel);
     sendWidget->setModel(walletModel);
 
+    // Connect actions..
+    connect(sendWidget, SIGNAL(message(QString, QString, unsigned int)), this, SLOT(message(QString, QString, unsigned int)));
+
 
     return true;
 }
