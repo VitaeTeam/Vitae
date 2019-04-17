f1e8ada96bffb9766f30b4e4d95759585f802a51
furszy <5377650+furszy@users.noreply.github.com>
2019-04-17 18:22:46 +0200

    wallet backup and change passphrase connected.
    # Conflicts:
    #	src/Makefile.qt.include
    #	src/qt/vitae/settings/forms/settingschangepasswordwidget.ui
    #	src/qt/vitae/settings/settingsbackupwallet.cpp
    #	src/qt/vitae/settings/settingschangepasswordwidget.cpp
    #	src/qt/vitae/settings/settingschangepasswordwidget.h
    #	src/qt/vitae/settings/settingswidget.cpp
    #	src/qt/vitae/settings/settingswidget.h

--- src/qt/vitae/settings/settingswidget.h
+++ src/qt/vitae/settings/settingswidget.h
@@ -6,7 +6,6 @@
 #include "qt/vitae/settings/settingsbackupwallet.h"
 #include "qt/vitae/settings/settingsbittoolwidget.h"
 #include "qt/vitae/settings/settingssignmessagewidgets.h"
-#include "qt/vitae/settings/settingschangepasswordwidget.h"
 #include "qt/vitae/settings/settingswalletrepairwidget.h"
 #include "qt/vitae/settings/settingsnetworkwidget.h"
 #include "qt/vitae/settings/settingswalletoptionswidget.h"
@@ -56,7 +55,6 @@ private slots:
     // Wallet Configuration
 
     void onConfigurationClicked();
-    void onChangePasswordClicked();
     void onBipToolClicked();
     void onMultisendClicked();
 
@@ -93,7 +91,6 @@ private:
     SettingsBackupWallet *settingsBackupWallet;
     SettingsBitToolWidget *settingsBitToolWidget;
     SettingsSignMessageWidgets *settingsSingMessageWidgets;
-    SettingsChangePasswordWidget *settingsChangePasswordWidget;
     SettingsWalletRepairWidget *settingsWalletRepairWidget;
     SettingsNetworkWidget *settingsNetworkWidget;
     SettingsWalletOptionsWidget *settingsWalletOptionsWidget;
