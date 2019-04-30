3b634fb4928468e1a8d79b0c5d9ca22adb54d5ed
random-zebra <random.zebra@protonmail.com>
2019-04-30 11:37:12 +0200

    [RPC] Disable zerocoin minting
    disabling also remint of change of a zc spend
    
    # Conflicts:
    #	src/rpcwallet.cpp

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -2792,6 +2792,8 @@ UniValue mintzerocoin(const UniValue& params, bool fHelp)
             "\nAs a json rpc call\n" +
             HelpExampleRpc("mintzerocoin", "13, \"[{\\\"txid\\\":\\\"a08e6907dbbd3d809776dbfc5d82e371b764ed838b5655e72f463568df1aadf0\\\",\\\"vout\\\":1}]\""));
 
+    throw JSONRPCError(RPC_WALLET_ERROR, "zPIV minting is DISABLED");
+    /*
     LOCK2(cs_main, pwalletMain->cs_wallet);
 
     if (params.size() == 1)
@@ -2860,6 +2862,7 @@ UniValue mintzerocoin(const UniValue& params, bool fHelp)
     }
 
     return arrMints;
+    */
 }
 
 UniValue spendzerocoin(const UniValue& params, bool fHelp)
@@ -2914,6 +2917,8 @@ UniValue spendzerocoin(const UniValue& params, bool fHelp)
 
     CAmount nAmount = AmountFromValue(params[0]);   // Spending amount
     bool fMintChange = params[1].get_bool();        // Mint change to zVITAE
+    if (fMintChange)
+        throw JSONRPCError(RPC_WALLET_ERROR, "zVIT minting is DISABLED, cannot mint change");
     bool fMinimizeChange = params[2].get_bool();    // Minimize change
     std::string address_str = params.size() > 3 ? params[3].get_str() : "";
     bool ispublicspend = params.size() > 4 ? params[3].get_bool() : true;
@@ -3024,6 +3029,10 @@ UniValue spendzerocoinmints(const UniValue& params, bool fHelp)
 
 extern UniValue DoZvitSpend(const CAmount nAmount, bool fMintChange, bool fMinimizeChange, vector<CZerocoinMint>& vMintsSelected, std::string address_str, bool ispublicspend)
 {
+    // zerocoin MINT is disabled. fMintChange should be false here. Double check
+    if (fMintChange)
+        throw JSONRPCError(RPC_WALLET_ERROR, "zPIV minting is DISABLED, cannot mint change");
+
     int64_t nTimeStart = GetTimeMillis();
     CBitcoinAddress address = CBitcoinAddress(); // Optional sending address. Dummy initialization here.
     CWalletTx wtx;
