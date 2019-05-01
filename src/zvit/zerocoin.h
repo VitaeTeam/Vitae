e4faa054078ecc92087c35cd8b6fa6f607e1b6a2
furszy <matiasfurszyfer@protonmail.com>
2019-05-01 11:44:58 +0200

    [UNIT TEST] publicCoinSpend valid input creation and verification.
    [UNIT TEST] publicCoinSpend creation + validation completed.

--- src/zvit/zerocoin.h
+++ src/zvit/zerocoin.h
@@ -42,6 +42,7 @@ private:
     CBigNum randomness;
     CBigNum serialNumber;
     uint256 txid;
+    int outputIndex;
     CPrivKey privkey;
     uint8_t version;
     bool isUsed;
@@ -102,6 +103,9 @@ public:
     CPrivKey GetPrivKey() const { return this->privkey; }
     void SetPrivKey(const CPrivKey& privkey) { this->privkey = privkey; }
     bool GetKeyPair(CKey& key) const;
+
+    unsigned int GetOutputIndex() { return this->outputIndex; }
+    void SetOutputIndex(unsigned int index) {this->outputIndex = index; }
 
     inline bool operator <(const CZerocoinMint& a) const { return GetHeight() < a.GetHeight(); }
 
@@ -185,6 +189,7 @@ private:
     libzerocoin::CoinDenomination denomination;
     unsigned int nAccumulatorChecksum;
     int nMintCount; //memory only - the amount of mints that belong to the accumulator this is spent from
+    bool publicSpend = false;
 
 public:
     CZerocoinSpend()
@@ -199,6 +204,16 @@ public:
         this->pubCoin = pubCoin;
         this->denomination = denomination;
         this->nAccumulatorChecksum = nAccumulatorChecksum;
+    }
+
+    CZerocoinSpend(CBigNum coinSerial, uint256 hashTx, CBigNum pubCoin, libzerocoin::CoinDenomination denomination, unsigned int nAccumulatorChecksum, bool isPublicSpend)
+    {
+        this->coinSerial = coinSerial;
+        this->hashTx = hashTx;
+        this->pubCoin = pubCoin;
+        this->denomination = denomination;
+        this->nAccumulatorChecksum = nAccumulatorChecksum;
+        this->publicSpend = isPublicSpend;
     }
 
     void SetNull()
@@ -218,6 +233,7 @@ public:
     uint256 GetHash() const;
     void SetMintCount(int nMintsAdded) { this->nMintCount = nMintsAdded; }
     int GetMintCount() const { return nMintCount; }
+    bool IsPublicSpend() const { return publicSpend; }
  
     ADD_SERIALIZE_METHODS;
 
