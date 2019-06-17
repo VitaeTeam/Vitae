940254fbccf63402e5cd31e0723f1335941b77e6
Fuzzbawls <fuzzbawls@gmail.com>
2019-06-17 03:28:39 -0700

    Merge #924: [Backport] Max tip age to consider a node in IBD status customizable.
    46ba7a2b96 [Backport] Max tip age to consider a node in IBD status customizable. Backport from bitcoin##7208. (furszy)
    
    Tree-SHA512: 211479d05cc8226d0bdaa3432204467679b465dfdbcdcf58650ec2fcb415c60544f3ae354f8b460adc6cd7e2d56ce6b3a52dd77ac92017eb7f66d98a76051bb2
    
    # Conflicts:
    #	src/init.cpp
    #	src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -97,6 +97,9 @@ unsigned int nCoinCacheSize = 5000;
 bool fAlerts = DEFAULT_ALERTS;
 bool fClearSpendCache = false;
 
+/* If the tip is older than this (in seconds), the node is considered to be in initial block download. */
+int64_t nMaxTipAge = DEFAULT_MAX_TIP_AGE;
+
 unsigned int nStakeMinAge = 1 * 60 * 60;
 int64_t nReserveBalance = 0;
 
@@ -2464,7 +2467,7 @@ bool IsInitialBlockDownload()
     if (lockIBDState)
         return false;
     bool state = (chainActive.Height() < pindexBestHeader->nHeight - 24 * 6 ||
-                  pindexBestHeader->GetBlockTime() < GetTime() - 6 * 60 * 60); // ~144 blocks behind -> 2 x fork detection time
+            pindexBestHeader->GetBlockTime() < GetTime() - nMaxTipAge);
     if (!state)
         lockIBDState = true;
     return state;
