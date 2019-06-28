b39f13acdf9df371da46965d4e10c6c41e9f5480
random-zebra <random.zebra@protonmail.com>
2019-06-28 17:37:26 +0200

    [zPIV] PublicCoinSpend v4 definition
    # Conflicts:
    #	src/wallet.cpp
    #	src/zvit/zvitmodule.cpp
    #	src/zvit/zvitmodule.h

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -4311,6 +4311,17 @@ bool CWallet::MintsToInputVectorPublicSpend(std::map<CBigNum, CZerocoinMint>& ma
     // Default error status if not changed below
     receipt.SetStatus(_("Transaction Mint Started"), ZVIT_TXMINT_GENERAL);
 
+    // Get the chain tip to determine the active public spend version
+    int nHeight = 0;
+    {
+        LOCK(cs_main);
+        nHeight = chainActive.Height();
+    }
+    if (!nHeight)
+        return error("%s: Unable to get chain tip height", __func__);
+
+    int spendVersion = Params().Zerocoin_PublicSpendVersion(nHeight);
+
     int nLockAttempts = 0;
     while (nLockAttempts < 100) {
         TRY_LOCK(zvitTracker->cs_spendcache, lockSpendcache);
@@ -4360,7 +4371,7 @@ bool CWallet::MintsToInputVectorPublicSpend(std::map<CBigNum, CZerocoinMint>& ma
 
             mint.SetOutputIndex(outputIndex);
             CTxIn in;
-            if(!ZVITModule::createInput(in, mint, hashTxOut)){
+            if(!ZVITModule::createInput(in, mint, hashTxOut, spendVersion)) {
                 receipt.SetStatus(_("Cannot create public spend input"), ZVIT_TXMINT_GENERAL);
                 return false;
             }
@@ -4582,6 +4593,7 @@ bool CWallet::CreateZerocoinSpendTransaction(
             }
 
             //hash with only the output info in it to be used in Signature of Knowledge
+            // and in CoinRandomness Schnorr Signature
             uint256 hashTxOut = txNew.GetHash();
 
             CBlockIndex* pindexCheckpoint = nullptr;
