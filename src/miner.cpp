774def0ef29b4040fb5d2598465f40d0d5c21a55
random-zebra <random.zebra@protonmail.com>
2019-08-18 15:22:01 +0200

    [Core] Introduce Blocks version 6
    [SQUASH] Cleanup proof-of-stake debug log
    
    # Conflicts:
    #	src/miner.cpp
    #	src/primitives/block.h

--- src/miner.cpp
+++ src/miner.cpp
@@ -122,6 +122,13 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
     // Make sure to create the correct block version after zerocoin is enabled
     bool fZerocoinActive = nHeight >= Params().Zerocoin_StartHeight();
     pblock->nVersion = 6;
+    /* from upstream, it's not suitable here since here already used version 6
+    if(Params().IsStakeModifierV2(nHeight)) {
+        pblock->nVersion = 6;       //!> Supports V2 Stake Modifiers.
+    } else {
+        pblock->nVersion = 5;       //!> Supports CLTV activation
+    }
+	*/
 
     // -regtest only: allow overriding block.nVersion with
     // -blockversion=N to test forking scenarios
