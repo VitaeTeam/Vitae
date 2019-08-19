e2fcbf66e722483a0d363919c1469af07fd4862e
furszy <matiasfurszyfer@protonmail.com>
2019-08-19 15:38:04 -0300

    [Wallet] Staking, unreacheable code removed..

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2499,8 +2499,6 @@ bool CWallet::CreateCoinStake(
             fKernelFound = true;
             break;
         }
-        if (fKernelFound)
-            break; // if kernel is found stop searching
     }
     LogPrint("staking", "%s: attempted staking %d times\n", __func__, nAttempts);
 
