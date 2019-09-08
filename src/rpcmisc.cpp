c4041170c916937ac00ea38b9000f49588905086
Akshay <akshaycm@hotmail.com>
2019-09-09 00:34:34 +0300

    remove forcesync

--- src/rpcmisc.cpp
+++ src/rpcmisc.cpp
@@ -142,7 +142,7 @@ UniValue fnsync(const UniValue& params, bool fHelp)
     if (params.size() == 1)
         strMode = params[0].get_str();
 
-    if (fHelp || params.size() != 1 || (strMode != "status" && strMode != "reset" && strMode != "forcesync")) {
+    if (fHelp || params.size() != 1 || (strMode != "status" && strMode != "reset")) {
         throw runtime_error(
             "fnsync \"status|reset\"\n"
             "\nReturns the sync status or resets sync.\n"
@@ -203,13 +203,6 @@ UniValue fnsync(const UniValue& params, bool fHelp)
         fundamentalnodeSync.Reset();
         return "success";
     }
-    if (strMode == "forcesync") {
-        for(int i = 0;i<=5;i++){
-          fundamentalnodeSync.GetNextAsset();
-        }
-        return fundamentalnodeSync.GetSyncStatus();
-    }
-
     return "failure";
 }
 
