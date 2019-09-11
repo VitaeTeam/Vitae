f283e28bc3768b4e075a655b23dd21a9e656f084
random-zebra <random.zebra@protonmail.com>
2019-09-11 17:06:17 +0200

    [PoS] Adjust FutureBlockTimeDrift to 15 secs granularity
    and remove nFutureTimeDriftPoS_V2

--- src/chainparams.h
+++ src/chainparams.h
@@ -189,7 +189,6 @@ protected:
     int nStakeMinDepth;
     int nFutureTimeDriftPoW;
     int nFutureTimeDriftPoS;
-    int nFutureTimeDriftPoS_V2;
     int nTimeSlotLength;
 
     int nModifierUpdateBlock;
