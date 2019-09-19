3bfa90050b3cfe453dfc06533ca948591536185b
random-zebra <random.zebra@protonmail.com>
2019-09-19 13:07:37 +0200

    Merge #1016: [GUI] Tx detail dialog, show seconds in toString date.
    03e3a65fc47cad380a51d6f08377f7aa2c2f8e68 [GUI] Tx detail dialog, show seconds in toString date. (furszy)
    
    Pull request description:
    
      More than descriptive title, transaction detail dialog showing the transaction time seconds now.
    
    ACKs for top commit:
      random-zebra:
        ACK 03e3a65fc47cad380a51d6f08377f7aa2c2f8e68
      Fuzzbawls:
        ACK 03e3a65fc47cad380a51d6f08377f7aa2c2f8e68
      Warrows:
        ACK 03e3a65
    
    Tree-SHA512: 6a9d9559f858e0dedf96fc69919de05a34561c3857e8147dbf6df5a2acd314b7ab3ee8642cecc609acbdcddbd4c3609c94253842ac5a778205342e491e225419

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -37,6 +37,7 @@ namespace GUIUtil
 {
 // Create human-readable string from date
 QString dateTimeStr(const QDateTime& datetime);
+QString dateTimeStrWithSeconds(const QDateTime& date);
 QString dateTimeStr(qint64 nTime);
 
 // Render VITAE addresses in monospace font
