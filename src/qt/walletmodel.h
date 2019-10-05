589f859a038d9175074333828f76b70df53d1b95
furszy <matiasfurszyfer@protonmail.com>
2019-10-04 23:58:45 -0300

    [Model] WalletModel P2CS transactions connected.

--- src/qt/walletmodel.h
+++ src/qt/walletmodel.h
@@ -52,6 +52,12 @@ public:
     QString label;
     AvailableCoinsType inputType;
     bool useSwiftTX = false;
+
+    // Cold staking.
+    bool isP2CS = false;
+    QString ownerAddress;
+
+    // Amount
     CAmount amount;
     // If from a payment request, this is used for storing the memo
     QString message;
@@ -117,7 +123,8 @@ public:
         TransactionCreationFailed, // Error returned when wallet is still locked
         TransactionCommitFailed,
         AnonymizeOnlyUnlocked,
-        InsaneFee
+        InsaneFee,
+        CannotCreateInternalAddress
     };
 
     enum EncryptionStatus {
@@ -207,6 +214,18 @@ public:
             bool fMinimizeChange,
             CZerocoinSpendReceipt &receipt
     );
+
+    // ###################
+    // Cold Staking
+    // ###################
+
+
+
+    // ###################
+    // End Cold Staking
+    // ###################
+
+
 
     // Wallet encryption
     bool setWalletEncrypted(bool encrypted, const SecureString& passphrase);
