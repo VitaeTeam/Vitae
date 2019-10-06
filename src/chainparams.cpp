e71cd5905b06d9c78407c4549b9d56d2fb90f5e0
random-zebra <random.zebra@protonmail.com>
2019-10-06 16:55:09 +0200

    [Tests] (squashed) few regtest fixes
    - fix regtest nStakeMinAge check
    - fix zerocoin_public_spend
    - fix StakeModifier V2 generation on regtest (0)
    - fix stake age on regtest
    # Conflicts:
    #	src/chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -109,13 +109,9 @@ libzerocoin::ZerocoinParams* CChainParams::Zerocoin_Params(bool useModulusV1) co
 bool CChainParams::HasStakeMinAgeOrDepth(const int contextHeight, const uint32_t contextTime,
         const int utxoFromBlockHeight, const uint32_t utxoFromBlockTime) const
 {
-    // Age not required on regtest
-    if (NetworkID() == CBaseChainParams::REGTEST)
-        return true;
-
-    // before stake modifier V2, the age required was 60 * 60 (1 hour)
+    // before stake modifier V2, the age required was 60 * 60 (1 hour). Not required for regtest
     if (!IsStakeModifierV2(contextHeight))
-        return (utxoFromBlockTime + 3600 <= contextTime);
+        return NetworkID() == CBaseChainParams::REGTEST || (utxoFromBlockTime + nStakeMinAge <= contextTime);
 
     // after stake modifier V2, we require the utxo to be nStakeMinDepth deep in the chain
     return (contextHeight - utxoFromBlockHeight >= nStakeMinDepth);
@@ -174,6 +170,7 @@ public:
         nTimeSlotLength = 15;                       // 15 seconds
         nTargetTimespan_V2 = 2 * nTimeSlotLength * 60;  // 15 minutes
         nMaturity = 8;
+        nStakeMinAge = 60 * 60;                         // 1 hour
         nStakeMinDepth = 600;
         nFundamentalnodeCountDrift = 20;
         nFutureTimeDriftPoW = 7200;
@@ -442,6 +439,7 @@ public:
         bnProofOfWorkLimit = ~uint256(0) >> 1;
         nLastPOWBlock = 250;
         nMaturity = 100;
+        nStakeMinAge = 0;
         nStakeMinDepth = 0;
         nTimeSlotLength = 1;            // time not masked on RegNet
         nMasternodeCountDrift = 4;
@@ -454,7 +452,7 @@ public:
         nBlockRecalculateAccumulators = 999999999; //Trigger a recalculation of accumulators
         nBlockFirstFraudulent = 999999999; //First block that bad serials emerged
         nBlockLastGoodCheckpoint = 999999999; //Last valid accumulator checkpoint
-        nBlockStakeModifierlV2 = 351;
+        nBlockStakeModifierlV2 = 255;
         nBlockTimeProtocolV2 = 999999999;
 
         // Public coin spend enforcement
