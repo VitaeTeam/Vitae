f047de91d4565874af0d50881cc16242c41b0c51
random-zebra <random.zebra@protonmail.com>
2019-11-20 12:39:19 +0100

    Merge #1110: [Masterndoes] Masternodes sync, try locking cs_main when it looks for the tip.
    455916108a5622cd873382ef830b80a11ffebb19 [Masternodes] Reset mn sync process if it's sleep. (furszy)
    21f278b4bdb14a9e21604878520d1ed7235ef882 [Masterndoes] Masternodes sync, try locking cs_main when it looks for the tip, preventing possible multi-threading shared resource problem. (furszy)
    
    Pull request description:
    
      Try locking cs_main when it looks for the tip, preventing possible shared resource issues.
    
    ACKs for top commit:
      random-zebra:
        utACK 455916108a5622cd873382ef830b80a11ffebb19
      Warrows:
        utACK 455916108a5622cd873382ef830b80a11ffebb19
    
    Tree-SHA512: cae5e9e5a4756656686a778ceecf1ab81ea06fc314fa7b334f71fbbf5a49d15c010bf63cb608ef78e57756eb32f50385690c60feba431ad8f9b95757694e4aed
    
    # Conflicts:
    #	src/obfuscation.cpp

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -52,28 +52,29 @@ bool CMasternodeSync::NotCompleted()
 
 bool CMasternodeSync::IsBlockchainSynced()
 {
-    static bool fBlockchainSynced = false;
-    static int64_t lastProcess = GetTime();
+    int64_t now = GetTime();
 
     // if the last call to this function was more than 60 minutes ago (client was in sleep mode) reset the sync process
-    if (GetTime() - lastProcess > 60 * 60) {
+    if (now > lastProcess + 60 * 60) {
         Reset();
         fBlockchainSynced = false;
     }
-    lastProcess = GetTime();
+    lastProcess = now;
 
     if (fBlockchainSynced) return true;
 
     if (fImporting || fReindex) return false;
 
-    TRY_LOCK(cs_main, lockMain);
-    if (!lockMain) return false;
+    int64_t blockTime = 0;
+    {
+        TRY_LOCK(cs_main, lockMain);
+        if (!lockMain) return false;
+        CBlockIndex *pindex = chainActive.Tip();
+        if (pindex == nullptr) return false;
+        blockTime = pindex->nTime;
+    }
 
-    CBlockIndex* pindex = chainActive.Tip();
-    if (pindex == NULL) return false;
-
-
-    if (pindex->nTime + 60 * 60 < GetTime())
+    if (blockTime + 60 * 60 < lastProcess)
         return false;
 
     fBlockchainSynced = true;
@@ -83,6 +84,8 @@ bool CMasternodeSync::IsBlockchainSynced()
 
 void CMasternodeSync::Reset()
 {
+    fBlockchainSynced = false;
+    lastProcess = 0;
     lastMasternodeList = 0;
     lastMasternodeWinner = 0;
     lastBudgetItem = 0;
@@ -330,7 +333,7 @@ void CMasternodeSync::Process()
                 if (lastMasternodeList == 0 &&
                     (RequestedMasternodeAttempt >= MASTERNODE_SYNC_THRESHOLD * 3 || GetTime() - nAssetSyncStarted > MASTERNODE_SYNC_TIMEOUT * 5)) {
                     if (sporkManager.IsSporkActive(SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT)) {
-                        LogPrintf("CMasternodeSync::Process - ERROR - Sync has failed, will retry later\n");
+                        LogPrintf("CMasternodeSync::Process - ERROR - Sync has failed on %s, will retry later\n", "MASTERNODE_SYNC_LIST");
                         RequestedMasternodeAssets = MASTERNODE_SYNC_FAILED;
                         RequestedMasternodeAttempt = 0;
                         lastFailure = GetTime();
@@ -361,7 +364,7 @@ void CMasternodeSync::Process()
                 if (lastMasternodeWinner == 0 &&
                     (RequestedMasternodeAttempt >= MASTERNODE_SYNC_THRESHOLD * 3 || GetTime() - nAssetSyncStarted > MASTERNODE_SYNC_TIMEOUT * 5)) {
                     if (sporkManager.IsSporkActive(SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT)) {
-                        LogPrintf("CMasternodeSync::Process - ERROR - Sync has failed, will retry later\n");
+                        LogPrintf("CMasternodeSync::Process - ERROR - Sync has failed on %s, will retry later\n", "MASTERNODE_SYNC_MNW");
                         RequestedMasternodeAssets = MASTERNODE_SYNC_FAILED;
                         RequestedMasternodeAttempt = 0;
                         lastFailure = GetTime();
@@ -373,9 +376,6 @@ void CMasternodeSync::Process()
                 }
 
                 if (RequestedMasternodeAttempt >= MASTERNODE_SYNC_THRESHOLD * 3) return;
-
-                CBlockIndex* pindexPrev = chainActive.Tip();
-                if (pindexPrev == NULL) return;
 
                 int nMnCount = mnodeman.CountEnabled();
                 pnode->PushMessage("mnget", nMnCount); //sync payees
