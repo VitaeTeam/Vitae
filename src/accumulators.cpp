5a9b6438b4ebe9e1867e160bc3fe9fb104f0fd59
akshay <akshaycm@hotmail.com>
2020-01-19 20:27:36 +0300

    add missing accumlatormap include

--- src/accumulators.cpp
+++ src/accumulators.cpp
@@ -3,7 +3,6 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "accumulators.h"
-#include "accumulatormap.h"
 #include "chainparams.h"
 #include "main.h"
 #include "txdb.h"
