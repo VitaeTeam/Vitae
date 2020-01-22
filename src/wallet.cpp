7cffa7ec132d557ff14c4e419a505ddc3719a8a2
random-zebra <random.zebra@protonmail.com>
2020-01-22 10:08:49 +0100

    Merge #1282: [Trivial][Cleanup] Add IsRegTestNet() function in chainparams
    c5a8adb3e4c51859eb41bbac8974f2751a46f867 [Core][Cleanup] Add IsRegTestNet() function in chainparams (random-zebra)
    
    Pull request description:
    
      This removes many explicit checks `Params().NetworkID() != CBaseChainParams::REGTEST` / `Params().NetworkID() == CBaseChainParams::REGTEST` making the code more readable.
    
    ACKs for top commit:
      Fuzzbawls:
        ACK c5a8adb3e4c51859eb41bbac8974f2751a46f867
    
    Tree-SHA512: ce6373be461c1db87cd48e4046c0c7429a57253a9fbd52d649f65e7ddb0385d0cf87b81f0538b97521f663f8ad79fbcc68f5e2bdbd9ca339ae8599d3d1cc38a6
    
    # Conflicts:
    #	src/activemasternode.cpp
    #	src/masternode.cpp
    #	src/masternodeman.cpp
    #	src/rpcrawtransaction.cpp
    #	src/rpcwallet.cpp
    #	src/wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -4023,7 +4023,7 @@ bool CWallet::CheckCoinSpend(libzerocoin::CoinSpend& spend, libzerocoin::Accumul
         return error("%s : The transaction did not verify", __func__);
     }
 
-    if (Params().NetworkID() != CBaseChainParams::REGTEST && IsSerialKnown(spend.getCoinSerialNumber())) {
+    if (IsSerialKnown(spend.getCoinSerialNumber())) {
         //Tried to spend an already spent zVIT
         receipt.SetStatus(_("The coin spend has been used"), ZVIT_SPENT_USED_ZVIT);
         uint256 hashSerial = GetSerialHash(spend.getCoinSerialNumber());
