phoreproject/Phore

PHR-7 adding  ask to unlock dialog in create proposal button

src/qt/proposallist.h
@@ -16,6 +16,7 @@
 #include <QWidget>
 
 class ProposalFilterProxy;
+class WalletModel;
 
 QT_BEGIN_NAMESPACE
 class QComboBox;
@@ -36,9 +37,11 @@ class ProposalList : public QWidget
     Q_OBJECT
 
 public:
-    explicit ProposalList(QWidget *parent = 0);
+    explicit ProposalList(QWidget* parent = 0);
 
     void setModel();
+    void setWalletModel(WalletModel* model);
+
 
     enum DateEnum
     {
@@ -77,6 +80,8 @@ private:
     QLineEdit *totalPaymentCountWidget;
     QLineEdit *remainingPaymentCountWidget;
     QTimer *timer;
+
+    WalletModel* walletModel;
 
     QLineEdit *yesVotesWidget;
     QLineEdit *noVotesWidget;

VitaeTeam/Vitae

src/qt/proposallist.h
@@ -16,6 +16,7 @@
 #include <QWidget>
 
 class ProposalFilterProxy;
+class WalletModel;
 
 QT_BEGIN_NAMESPACE
 class QComboBox;
@@ -38,7 +39,7 @@ class ProposalList : public QWidget
 public:
     explicit ProposalList(QWidget *parent = 0);
 
-    void setModel();
+    void setModel(WalletModel* model);
 
     enum DateEnum
     {
@@ -66,6 +67,7 @@ public:
     };
 
 private:
+    WalletModel* model;
     ProposalTableModel *proposalTableModel;
     ProposalFilterProxy *proposalProxyModel;
     QTableView *proposalList;