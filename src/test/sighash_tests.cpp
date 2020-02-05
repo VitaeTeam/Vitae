be3024c8027a1edd5dec40812be9add29e3a8ef1
random-zebra <random.zebra@protonmail.com>
2020-02-05 12:29:21 +0100

    Merge #1320: [Refactor] Move transaction checks out to consensus/tx_verify.cpp
    1e0bc16f5c958bf767e6465593d1cf794755d618 Move transaction checks out to consensus/tx_verify.cpp. (barrystyle)
    
    Pull request description:
    
      This is PR is the third part of breaking up #1209 into individual narrow-focused PRs. Original commit(s) were cherry-picked from @barrystyle's initial pre-squashed branch and then re-squashed (reference branch is at https://github.com/Fuzzbawls/PIVX/commits/pr-1209)
    
      Input commits:
      https://github.com/Fuzzbawls/PIVX/commit/90950f18ea2553c0e21aebd58ecb73d1f521da59
      https://github.com/Fuzzbawls/PIVX/commit/10b756628297d7ce5e73b7461af12993c5326fd4
    
      ---------------
    
      This Moves the basic (non-zerocoin) transaction validation checks away from main.h(cpp) and into the `consensus` subdir. Code move only, no changes to functionality.
    
      This PR conflicts minorly with #1319, and will need to be rebased once that PR is merged, but reviewing is possible now as the conflicts are mostly just matter-of-fact conflicts due to both PRs touching a common file.
    
      Relies on:
      - [x] #1319
    
    ACKs for top commit:
      furszy:
        utACK 1e0bc16
      random-zebra:
        utACK 1e0bc16f5c958bf767e6465593d1cf794755d618 and merging...
    
    Tree-SHA512: aa82d6f473d7ff6b65f04e590eb5dcfbcb5c1f22635bc22f0204ef3c23adcca99f95fc16d0b838c7358d6b832a4c55d43bf5f255902ea7fea4f5ff6216ea1423

--- src/test/sighash_tests.cpp
+++ src/test/sighash_tests.cpp
@@ -5,6 +5,8 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "data/sighash.json.h"
+
+#include "consensus/tx_verify.h"
 #include "main.h"
 #include "serialize.h"
 #include "script/script.h"
