5724c9910c3c865b2170dd84dcc49bc3d2d9e581
furszy <matiasfurszyfer@protonmail.com>
2020-02-11 21:19:10 -0300

    [Cleanup] Removing unused NewKeyPool from the wallet class.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -452,7 +452,6 @@ public:
     static CFeeRate minTxFee;
     static CAmount GetMinimumFee(unsigned int nTxBytes, unsigned int nConfirmTarget, const CTxMemPool& pool);
 
-    bool NewKeyPool();
     size_t KeypoolCountExternalKeys();
     bool TopUpKeyPool(unsigned int kpSize = 0);
     // TODO: Cleanup the follow methods, all are implemented inside the SPKM.
