68f43f327fb2c96e79882bf8a092bd1ac8722493
furszy <matiasfurszyfer@protonmail.com>
2020-02-17 20:36:32 -0300

    Merge #1332: [GUI] Do not update the GUI so often when reindex/import is being executed.
    bd9abcf9ebe20efdc4a19fab98a1b8762538a12b fReindex and fImporting moved to atomic bool. (furszy)
    617a87da4d410179b2c6e4829fbc096ee8d6b93a [GUI] Do not update the GUI so often when reindex/import is being executed. (furszy)
    
    Pull request description:
    
      auto-descriptive title.
    
      An increased delay time between GUI polling updates when reindex/import is being executed.
    
    ACKs for top commit:
      random-zebra:
        ACK bd9abcf9ebe20efdc4a19fab98a1b8762538a12b
      Mrs-X:
        ACK https://github.com/PIVX-Project/PIVX/pull/1332/commits/bd9abcf9ebe20efdc4a19fab98a1b8762538a12b
    
    Tree-SHA512: dc65951e68e67fd027b9cdd799e4b907695cd2e97c1095c3664c4bd5a9b9525c273584a98b078c15d47f86da16fa7b29e9736a25c98f31f0657f85e294c412dd

--- src/main.h
+++ src/main.h
@@ -37,6 +37,7 @@
 #include "undo.h"
 
 #include <algorithm>
+#include <atomic>
 #include <exception>
 #include <map>
 #include <set>
@@ -135,8 +136,8 @@ extern CWaitableCriticalSection g_best_block_mutex;
 extern std::condition_variable g_best_block_cv;
 extern uint256 g_best_block;
 
-extern bool fImporting;
-extern bool fReindex;
+extern std::atomic<bool> fImporting;
+extern std::atomic<bool> fReindex;
 extern int nScriptCheckThreads;
 extern bool fTxIndex;
 extern bool fIsBareMultisigStd;
