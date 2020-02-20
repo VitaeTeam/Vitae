862ba1447dc586b7123e2773c85b024797c4aae3
random-zebra <random.zebra@protonmail.com>
2020-02-20 10:00:01 +0100

    Merge #1343: [Bug] nStakeSplitThreshold: division by zero
    4829b353e05beed9cf27ecdf908bccb5973a1ced [Bug] nStakeSplitThreshold: division by zero (random-zebra)
    
    Pull request description:
    
      RPC should accept only values between 1 and 999999 (as per `setstakesplitthreshold` help) but the input provided was only checked against the upper bound.
      Setting a 0 value would result in a crash (division-by-zero) at the first stake.
    
      Now prevent the wallet from splitting the stake if the threshold is set to zero.
    
      Future work:
      There is no reason why the split threshold should be an integer.
      Make it `CAmount` and enable decimal values for it.
      Also use `JSONRPCError(RPC_INVALID_PARAMETER)` instead of runtime errors when the input is not valid.
    
    ACKs for top commit:
      CaveSpectre11:
        utACK https://github.com/PIVX-Project/PIVX/commit/4829b353e05beed9cf27ecdf908bccb5973a1ced
      furszy:
        Good catch, utACK 4829b35
      Fuzzbawls:
        utACK 4829b353e05beed9cf27ecdf908bccb5973a1ced
    
    Tree-SHA512: 624f3d5a100c07b5706c684d54da959d1175d32156e2fa82e0033244bce56ebb567ec93c545010a1b84cd4117fc20ad060bab078db98f71942b03a2bdf04725c

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -2800,7 +2800,7 @@ UniValue setstakesplitthreshold(const UniValue& params, bool fHelp)
             HelpRequiringPassphrase() + "\n"
 
             "\nArguments:\n"
-            "1. value   (numeric, required) Threshold value between 1 and 999999\n"
+            "1. value   (numeric, required) Threshold value between 1 and 999999 or 0 to disable stake-splitting\n"
 
             "\nResult:\n"
             "{\n"
@@ -2814,6 +2814,9 @@ UniValue setstakesplitthreshold(const UniValue& params, bool fHelp)
     EnsureWalletIsUnlocked();
 
     uint64_t nStakeSplitThreshold = params[0].get_int();
+
+    if (nStakeSplitThreshold < 0)
+        throw std::runtime_error("Value out of range, min allowed is 0");
 
     if (nStakeSplitThreshold > 999999)
         throw std::runtime_error("Value out of range, max allowed is 999999");
