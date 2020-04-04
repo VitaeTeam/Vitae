6f2894142730eb31fe5ca1f53faadeaa6bd891d6
wqking <wqking@outlook.com>
2020-04-04 14:59:25 +0800

    Fixed compile error

--- src/zvit/accumulators.h
+++ src/zvit/accumulators.h
@@ -8,7 +8,7 @@
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Coin.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zerocoin.h"
 #include "accumulatormap.h"
 #include "chain.h"
 #include "uint256.h"
--- src/zvit/mintpool.h
+++ src/zvit/mintpool.h
@@ -8,7 +8,7 @@
 #include <map>
 #include <list>
 
-#include "primitives/zerocoin.h"
+#include "zvit/zerocoin.h"
 #include "libzerocoin/bignum.h"
 #include "uint256.h"
 
