fc941f7438b1166ca0b176261dddc51b2a0b8857
random-zebra <random.zebra@protonmail.com>
2020-04-06 00:38:46 +0200

    [zPIV] Remove extra on-time recalculation of supply
    on height_ZC_RecalcAccumulators, without a supply checkpoint.
    Use wrapped serials check instead.
    # Conflicts:
    #	src/consensus/zerocoin_verify.cpp
    #	src/main.cpp

--- src/consensus/zerocoin_verify.cpp
+++ src/consensus/zerocoin_verify.cpp
@@ -201,6 +201,14 @@ bool RecalculateVITSupply(int nHeightStart, bool fSkipZpiv)
     if (nHeightStart == consensus.height_start_ZC)
         nSupplyPrev = CAmount(649916627717750);
 
+        nSupplyPrev = CAmount(5449796547496199);
+
+    if (!fSkipZpiv) {
+        // initialize supply to 0
+        mapZerocoinSupply.clear();
+        for (auto& denom : libzerocoin::zerocoinDenomList) mapZerocoinSupply.insert(std::make_pair(denom, 0));
+    }
+
     uiInterface.ShowProgress(_("Recalculating VIT supply..."), 0);
     while (true) {
         if (pindex->nHeight % 1000 == 0) {
