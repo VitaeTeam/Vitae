402f180b970131ae18ee27a99402a5f9cbbd72ee
wqking <wqking@outlook.com>
2020-04-09 20:03:08 +0800

    Updated big number from upstream

--- src/test/zerocoin_bignum_tests.cpp
+++ src/test/zerocoin_bignum_tests.cpp
@@ -8,7 +8,7 @@
 #include "libzerocoin/Denominations.h"
 #include "libzerocoin/CoinSpend.h"
 #include "libzerocoin/Accumulator.h"
-#include "zpiv/zerocoin.h"
+#include "zvit/zerocoin.h"
 
 using namespace libzerocoin;
 
