a5f83f34b98aa96f050745d16d3ce054f4b90bd5
Crypto Pliskin <13294167+2a5A1Ghu1@users.noreply.github.com>
2020-05-28 16:33:28 -0700

    remove seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -175,11 +175,6 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         vSeeds.push_back(CDNSSeedData("209.182.216.144", "209.182.216.144")); // vitae fn
-        vSeeds.push_back(CDNSSeedData("198.13.50.121", "198.13.50.121"));     // rasalghul supernode
-        vSeeds.push_back(CDNSSeedData("104.238.183.75", "104.238.183.75"));   // rasalghul masternode
-        vSeeds.push_back(CDNSSeedData("159.89.227.25", "159.89.227.25"));     // squidicuz supernode
-        vSeeds.push_back(CDNSSeedData("217.163.28.214", "217.163.28.214"));   // explorer
-        vSeeds.push_back(CDNSSeedData("185.24.97.11", "185.24.97.11"));       // explorer
         vSeeds.push_back(CDNSSeedData("140.82.24.121", "140.82.24.121"));     // masternode
         vSeeds.push_back(CDNSSeedData("144.202.78.55", "144.202.78.55"));     // masternode
         vSeeds.push_back(CDNSSeedData("45.32.230.157", "45.32.230.157"));     // supernode
