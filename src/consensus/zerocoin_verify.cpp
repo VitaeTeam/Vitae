RapidsOfficial/Rapids

commit df932b8ff85c1e668f5fea7169b0e6256b864546

[Core][Cleanup] CBlockIndex: remove vMintDenominationsInBlock
- remove RecalculateZPIVSpent and RecalculateZPIVMinted main functions
- remove MintedDenomination CBlockIndex method

src/init.cpp
@@ -1543,10 +1543,6 @@ bool AppInit2()
 
                 // Recalculate money supply for blocks that are impacted by accounting issue after zerocoin activation
                 if (GetBoolArg("-reindexmoneysupply", false) || reindexZerocoin) {
-                    if (chainHeight > Params().Zerocoin_StartHeight()) {
-                        RecalculateZPIVMinted();
-                        RecalculateZPIVSpent();
-                    }
                     // Recalculate from the zerocoin activation or from scratch.
                     RecalculatePIVSupply(reindexZerocoin ? Params().Zerocoin_StartHeight() : 1);
                 }

src/chain.cpp
@@ -103,7 +103,6 @@ void CBlockIndex::SetNull()
     // Start supply of each denomination with 0s
     for (auto& denom : libzerocoin::zerocoinDenomList)
         mapZerocoinSupply.insert(std::make_pair(denom, 0));
-    vMintDenominationsInBlock.clear();
 }
 
 std::string CBlockIndex::ToString() const
@@ -287,7 +286,3 @@ int64_t CBlockIndex::GetZcMintsAmount(libzerocoin::CoinDenomination denom) const
     return libzerocoin::ZerocoinDenominationToAmount(denom) * GetZcMints(denom);
 }
 
-bool CBlockIndex::MintedDenomination(libzerocoin::CoinDenomination denom) const
-{
-    return std::find(vMintDenominationsInBlock.begin(), vMintDenominationsInBlock.end(), denom) != vMintDenominationsInBlock.end();
-}

src/chain.h
@@ -234,7 +234,6 @@ public:
 
     //! zerocoin specific fields
     std::map<libzerocoin::CoinDenomination, int64_t> mapZerocoinSupply;
-    std::vector<libzerocoin::CoinDenomination> vMintDenominationsInBlock;
 
     CBlockIndex() { SetNull(); }
     CBlockIndex(const CBlock& block);
@@ -287,7 +286,6 @@ public:
     int64_t GetZerocoinSupply() const;
     int64_t GetZcMints(libzerocoin::CoinDenomination denom) const;
     int64_t GetZcMintsAmount(libzerocoin::CoinDenomination denom) const;
-    bool MintedDenomination(libzerocoin::CoinDenomination denom) const;
 };
 
 /** Used to marshal pointers into hashes for db storage. */
@@ -333,15 +331,10 @@ public:
         if (nStatus & BLOCK_HAVE_UNDO)
             READWRITE(VARINT(nUndoPos));
 
-        if (fOldSer) {
-            int64_t nMint = 0;
-            READWRITE(nMint);
-        }
-        READWRITE(nMoneySupply);
-        READWRITE(nFlags);
-
         if (fNewSer) {
             // Serialization with client version > 4009900
+            READWRITE(nMoneySupply);
+            READWRITE(nFlags);
             READWRITE(vStakeModifier);
             // block header
             READWRITE(this->nVersion);
@@ -353,7 +346,6 @@ public:
             if(this->nVersion > 3) {
                 READWRITE(mapZerocoinSupply);
                 if(this->nVersion < 7) READWRITE(nAccumulatorCheckpoint);
-                READWRITE(vMintDenominationsInBlock);
             }
 
         } else {
@@ -381,6 +373,7 @@ public:
             READWRITE(nBits);
             READWRITE(nNonce);
             if(this->nVersion > 3) {
+                std::vector<libzerocoin::CoinDenomination> vMintDenominationsInBlock;
                 READWRITE(nAccumulatorCheckpoint);
                 READWRITE(mapZerocoinSupply);
                 READWRITE(vMintDenominationsInBlock);

src/main.cpp
@@ -2673,87 +2673,6 @@ void AddWrappedSerialsInflation()
     uiInterface.ShowProgress("", 100);
 }
 
-void RecalculateZPIVMinted()
-{
-    CBlockIndex *pindex = chainActive[Params().Zerocoin_StartHeight()];
-    const int chainHeight = chainActive.Height();
-    uiInterface.ShowProgress(_("Recalculating minted ZPIV..."), 0);
-    while (true) {
-        // Log Message and feedback message every 1000 blocks
-        if (pindex->nHeight % 1000 == 0) {
-            LogPrintf("%s : block %d...\n", __func__, pindex->nHeight);
-            int percent = std::max(1, std::min(99, (int)((double)(pindex->nHeight - Params().Zerocoin_StartHeight()) * 100 / (chainHeight - Params().Zerocoin_StartHeight()))));
-            uiInterface.ShowProgress(_("Recalculating minted ZPIV..."), percent);
-        }
-
-        //overwrite possibly wrong vMintsInBlock data
-        CBlock block;
-        assert(ReadBlockFromDisk(block, pindex));
-
-        std::list<CZerocoinMint> listMints;
-        BlockToZerocoinMintList(block, listMints, true);
-
-        std::vector<libzerocoin::CoinDenomination> vDenomsBefore = pindex->vMintDenominationsInBlock;
-        pindex->vMintDenominationsInBlock.clear();
-        for (auto mint : listMints)
-            pindex->vMintDenominationsInBlock.emplace_back(mint.GetDenomination());
-
-        if (pindex->nHeight < chainHeight)
-            pindex = chainActive.Next(pindex);
-        else
-            break;
-    }
-    uiInterface.ShowProgress("", 100);
-}
-
-void RecalculateZPIVSpent()
-{
-    CBlockIndex* pindex = chainActive[Params().Zerocoin_StartHeight()];
-    const int chainHeight = chainActive.Height();
-    uiInterface.ShowProgress(_("Recalculating spent ZPIV..."), 0);
-    while (true) {
-        if (pindex->nHeight % 1000 == 0) {
-            LogPrintf("%s : block %d...\n", __func__, pindex->nHeight);
-            int percent = std::max(1, std::min(99, (int)((double)(pindex->nHeight - Params().Zerocoin_StartHeight()) * 100 / (chainHeight - Params().Zerocoin_StartHeight()))));
-            uiInterface.ShowProgress(_("Recalculating spent ZPIV..."), percent);
-        }
-
-        //Rewrite zPIV supply
-        CBlock block;
-        assert(ReadBlockFromDisk(block, pindex));
-
-        std::list<libzerocoin::CoinDenomination> listDenomsSpent = ZerocoinSpendListFromBlock(block, true);
-
-        //Reset the supply to previous block
-        pindex->mapZerocoinSupply = pindex->pprev->mapZerocoinSupply;
-
-        //Add mints to zPIV supply
-        for (auto denom : libzerocoin::zerocoinDenomList) {
-            long nDenomAdded = count(pindex->vMintDenominationsInBlock.begin(), pindex->vMintDenominationsInBlock.end(), denom);
-            pindex->mapZerocoinSupply.at(denom) += nDenomAdded;
-        }
-
-        //Remove spends from zPIV supply
-        for (auto denom : listDenomsSpent)
-            pindex->mapZerocoinSupply.at(denom)--;
-
-        // Add inflation from Wrapped Serials if block is Zerocoin_Block_EndFakeSerial()
-        if (pindex->nHeight == Params().Zerocoin_Block_EndFakeSerial() + 1)
-            for (auto denom : libzerocoin::zerocoinDenomList) {
-                pindex->mapZerocoinSupply.at(denom) += GetWrapppedSerialInflation(denom);
-            }
-
-        //Rewrite money supply
-        assert(pblocktree->WriteBlockIndex(CDiskBlockIndex(pindex)));
-
-        if (pindex->nHeight < chainHeight)
-            pindex = chainActive.Next(pindex);
-        else
-            break;
-    }
-    uiInterface.ShowProgress("", 100);
-}
-
 bool RecalculatePIVSupply(int nHeightStart)
 {
     const int chainHeight = chainActive.Height();
@@ -2846,12 +2765,10 @@ bool UpdateZPIVSupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck)
 
     // Track zerocoin money supply
     CAmount nAmountZerocoinSpent = 0;
-    pindex->vMintDenominationsInBlock.clear();
     if (pindex->pprev) {
         std::set<uint256> setAddedToWallet;
         for (auto& m : listMints) {
             libzerocoin::CoinDenomination denom = m.GetDenomination();
-            pindex->vMintDenominationsInBlock.push_back(m.GetDenomination());
             pindex->mapZerocoinSupply.at(denom)++;
 
             //Remove any of our own mints from the mintpool
@@ -3095,8 +3012,6 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
 
     //A one-time event where money supply counts were off and recalculated on a certain block.
     if (pindex->nHeight == Params().Zerocoin_Block_RecalculateAccumulators() + 1) {
-        RecalculateZPIVMinted();
-        RecalculateZPIVSpent();
         RecalculatePIVSupply(Params().Zerocoin_StartHeight());
     }
 

src/main.h
@@ -290,8 +290,6 @@ bool IsTransactionInChain(const uint256& txId, int& nHeightTx);
 bool IsBlockHashInChain(const uint256& hashBlock);
 bool ValidOutPoint(const COutPoint& out, int nHeight);
 void AddWrappedSerialsInflation();
-void RecalculateZPIVSpent();
-void RecalculateZPIVMinted();
 bool RecalculatePIVSupply(int nHeightStart);
 
 // Fake Serial attack Range

src/txdb.cpp
@@ -216,7 +216,6 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
     pcursor->Seek(ssKeySet.str());
 
     // Load mapBlockIndex
-    uint256 nPreviousCheckpoint;
     while (pcursor->Valid()) {
         boost::this_thread::interruption_point();
         try {
@@ -249,7 +248,6 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
                 //zerocoin
                 pindexNew->nAccumulatorCheckpoint = diskindex.nAccumulatorCheckpoint;
                 pindexNew->mapZerocoinSupply = diskindex.mapZerocoinSupply;
-                pindexNew->vMintDenominationsInBlock = diskindex.vMintDenominationsInBlock;
 
                 //Proof Of Stake
                 pindexNew->nMoneySupply = diskindex.nMoneySupply;

src/wallet/rpcwallet.cpp
@@ -4313,19 +4313,17 @@ UniValue spendrawzerocoin(const UniValue& params, bool fHelp)
             CBlockIndex* pindex = chainActive.Tip();
             while (!found && pindex && pindex->nHeight >= Params().Zerocoin_StartHeight()) {
                 LogPrintf("%s : Checking block %d...\n", __func__, pindex->nHeight);
-                if (pindex->MintedDenomination(denom)) {
-                    CBlock block;
-                    if (!ReadBlockFromDisk(block, pindex))
-                        throw JSONRPCError(RPC_INTERNAL_ERROR, "Unable to read block from disk");
-                    std::list<CZerocoinMint> listMints;
-                    BlockToZerocoinMintList(block, listMints, true);
-                    for (const CZerocoinMint& m : listMints) {
-                        if (m.GetValue() == mintValue && m.GetDenomination() == denom) {
-                            // mint found. update txid
-                            mint.SetTxHash(m.GetTxHash());
-                            found = true;
-                            break;
-                        }
+                CBlock block;
+                if (!ReadBlockFromDisk(block, pindex))
+                    throw JSONRPCError(RPC_INTERNAL_ERROR, "Unable to read block from disk");
+                std::list<CZerocoinMint> listMints;
+                BlockToZerocoinMintList(block, listMints, true);
+                for (const CZerocoinMint& m : listMints) {
+                    if (m.GetValue() == mintValue && m.GetDenomination() == denom) {
+                        // mint found. update txid
+                        mint.SetTxHash(m.GetTxHash());
+                        found = true;
+                        break;
                     }
                 }
                 pindex = pindex->pprev;

VitaeTeam/Vitae

commit 0907d505df3ccbeb2bb6d095b1151304dce00f1b

Merge #1359: [Chain][Bug] Fix mapZerocoinSupply recalculation
ecc649994788cd331b8c000b2fa252647ca2a75d [Chain] Update zpiv supply in RecalculatePIVSupply (random-zebra)
9f072a93354c86ab2195360eb2f300d32dc44cc5 [Bug] Always filter invalid in UpdateZPIVSupply (random-zebra)

Pull request description:

  df932b8ff85c1e668f5fea7169b0e6256b864546 introduced a bug preventing resync on mainnet due to zerocoin supply recalculation missing.
  ZPIV supply recalculation shouldn't be needed in the first place, if the client is syncing from scratch and correctly filtering invalid mints.
  So, instead of restoring `RecalculateZPIVMinted`/`RecalculateZPIVSpent`, this PR does 2 things:

  - fixes `UpdateZPIVSupply` filtering the invalid mints/spends since the start of zerocoin (not only after `Zerocoin_Block_RecalculateAccumulators`). So, syncing from scratch doesn not trigger any recalculation at the next startup.

  - inserts a call to `UpdateZPIVSupply` directly in `RecalculatePIVSupply`, to be used during init, e.g. by those clients updating from old versions (with wrong supply in the index), or when using `-reindexmoneysupply`.
  This removes the additional I/O operations that were originally required with `RecalculateZPIVMinted` and `RecalculateZPIVSpent` (as blocks are already read inside `RecalculatePIVSupply`, and the relative indexes also already updated there).

ACKs for top commit:
  Fuzzbawls:
    ACK ecc649994788cd331b8c000b2fa252647ca2a75d
  furszy:
    tested ACK ecc6499

Tree-SHA512: 541b9f60425f3ff17b0b2589e43acc757a2e8385f8f96dd0a764e2f76b134a65c47324acdc482547ef59722c8c88e9f615e6caf6ffc910d8dd344b34e2850954

# Conflicts:
#	src/consensus/zerocoin_verify.cpp
#	src/consensus/zerocoin_verify.h
#	src/init.cpp

src/consensus/zerocoin_verify.cpp
@@ -149,8 +149,9 @@ bool ContextualCheckZerocoinSpend(const CTransaction& tx, const libzerocoin::Coi
 
 bool ContextualCheckZerocoinSpendNoSerialCheck(const CTransaction& tx, const libzerocoin::CoinSpend* spend, int nHeight, const uint256& hashBlock)
 {
+    const Consensus::Params& consensus = Params().GetConsensus();
     //Check to see if the zVIT is properly signed
-    if (nHeight >= Params().GetConsensus().height_start_ZC_SerialsV2) {
+    if (nHeight >= consensus.height_start_ZC_SerialsV2) {
         try {
             if (!spend->HasValidSignature())
                 return error("%s: V2 zVIT spend does not have a valid signature\n", __func__);
@@ -174,7 +175,7 @@ bool ContextualCheckZerocoinSpendNoSerialCheck(const CTransaction& tx, const lib
     bool fUseV1Params = spend->getCoinVersion() < libzerocoin::PrivateCoin::PUBKEY_VERSION;
 
     //Reject serial's that are not in the acceptable value range
-    if (!spend->HasValidSerial(Params().GetConsensus().Zerocoin_Params(fUseV1Params)))  {
+    if (!spend->HasValidSerial(consensus.Zerocoin_Params(fUseV1Params)))  {
         // Up until this block our chain was not checking serials correctly..
         if (!isBlockBetweenFakeSerialAttackRange(nHeight))
             return error("%s : zVIT spend with serial %s from tx %s is not in valid range\n", __func__,
@@ -218,15 +219,16 @@ void AddWrappedSerialsInflation()
     uiInterface.ShowProgress("", 100);
 }
 
-bool RecalculateVITSupply(int nHeightStart)
+bool RecalculateVITSupply(int nHeightStart, bool fSkipZpiv)
 {
+    const Consensus::Params& consensus = Params().GetConsensus();
     const int chainHeight = chainActive.Height();
     if (nHeightStart > chainHeight)
         return false;
 
     CBlockIndex* pindex = chainActive[nHeightStart];
     CAmount nSupplyPrev = pindex->pprev->nMoneySupply;
-    if (nHeightStart == Params().GetConsensus().height_start_ZC)
+    if (nHeightStart == consensus.height_start_ZC)
         nSupplyPrev = CAmount(649916627717750);
 
     uiInterface.ShowProgress(_("Recalculating VIT supply..."), 0);
@@ -271,8 +273,13 @@ bool RecalculateVITSupply(int nHeightStart)
         pindex->nMoneySupply = nSupplyPrev + nValueOut - nValueIn;
         nSupplyPrev = pindex->nMoneySupply;
 
+        // Rewrite zpiv supply too
+        if (!fSkipZpiv && pindex->nHeight >= consensus.height_start_ZC) {
+            UpdateZPIVSupply(block, pindex, true);
+        }
+
         // Add fraudulent funds to the supply and remove any recovered funds.
-        if (pindex->nHeight == Params().GetConsensus().height_ZC_RecalcAccumulators) {
+        if (pindex->nHeight == consensus.height_ZC_RecalcAccumulators) {
             const CAmount nInvalidAmountFiltered = 268200*COIN;    //Amount of invalid coins filtered through exchanges, that should be considered valid
             LogPrintf("%s : Original money supply=%s\n", __func__, FormatMoney(pindex->nMoneySupply));
 
@@ -286,6 +293,9 @@ bool RecalculateVITSupply(int nHeightStart)
 
         assert(pblocktree->WriteBlockIndex(CDiskBlockIndex(pindex)));
 
+        // Stop if shutdown was requested
+        if (ShutdownRequested()) return false;
+
         if (pindex->nHeight < chainHeight)
             pindex = chainActive.Next(pindex);
         else
@@ -297,31 +307,24 @@ bool RecalculateVITSupply(int nHeightStart)
 
 bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck)
 {
-    std::list<CZerocoinMint> listMints;
-    bool fFilterInvalid = pindex->nHeight >= Params().GetConsensus().height_ZC_RecalcAccumulators;
-    BlockToZerocoinMintList(block, listMints, fFilterInvalid);
-    std::list<libzerocoin::CoinDenomination> listSpends = ZerocoinSpendListFromBlock(block, fFilterInvalid);
+    const Consensus::Params& consensus = Params().GetConsensus();
+    if (pindex->nHeight < consensus.height_start_ZC)
+        return true;
 
-    // Initialize zerocoin supply to the supply from previous block
-    if (pindex->pprev && pindex->pprev->GetBlockHeader().nVersion > 3) {
-        for (auto& denom : libzerocoin::zerocoinDenomList) {
-            pindex->mapZerocoinSupply.at(denom) = pindex->pprev->GetZcMints(denom);
-        }
-    }
+    //Reset the supply to previous block
+    pindex->mapZerocoinSupply = pindex->pprev->mapZerocoinSupply;
 
-    // Track zerocoin money supply
-    CAmount nAmountZerocoinSpent = 0;
-    if (pindex->pprev) {
+    //Add mints to zPIV supply (mints are forever disabled after last checkpoint)
+    if (pindex->nHeight < consensus.height_last_ZC_AccumCheckpoint) {
+        std::list<CZerocoinMint> listMints;
         std::set<uint256> setAddedToWallet;
-        for (auto& m : listMints) {
-            libzerocoin::CoinDenomination denom = m.GetDenomination();
-            pindex->mapZerocoinSupply.at(denom)++;
-
+        BlockToZerocoinMintList(block, listMints, true);
+        for (const auto& m : listMints) {
+            pindex->mapZerocoinSupply.at(m.GetDenomination())++;
             //Remove any of our own mints from the mintpool
             if (!fJustCheck && pwalletMain) {
                 if (pwalletMain->IsMyMint(m.GetValue())) {
                     pwalletMain->UpdateMint(m.GetValue(), pindex->nHeight, m.GetTxHash(), m.GetDenomination());
-
                     // Add the transaction to the wallet
                     for (auto& tx : block.vtx) {
                         uint256 txid = tx.GetHash();
@@ -338,29 +341,28 @@ bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex, bool fJustChec
                 }
             }
         }
-
-        for (auto& denom : listSpends) {
-            pindex->mapZerocoinSupply.at(denom)--;
-            nAmountZerocoinSpent += libzerocoin::ZerocoinDenominationToAmount(denom);
-
-            // zerocoin failsafe
-            if (pindex->GetZcMints(denom) < 0)
-                return error("Block contains zerocoins that spend more than are in the available supply to spend");
-        }
     }
 
-    for (auto& denom : libzerocoin::zerocoinDenomList)
-        LogPrint("zero", "%s coins for denomination %d pubcoin %s\n", __func__, denom, pindex->mapZerocoinSupply.at(denom));
+    //Remove spends from zPIV supply
+    std::list<libzerocoin::CoinDenomination> listDenomsSpent = ZerocoinSpendListFromBlock(block, true);
+    for (const auto& denom : listDenomsSpent) {
+        pindex->mapZerocoinSupply.at(denom)--;
+        // zerocoin failsafe
+        if (pindex->mapZerocoinSupply.at(denom) < 0)
+            return error("Block contains zerocoins that spend more than are in the available supply to spend");
+    }
 
     // Update Wrapped Serials amount
-    // A one-time event where only the zVIT supply was off (due to serial duplication off-chain on main net)
-    const Consensus::Params& consensus = Params().GetConsensus();
+    // A one-time event where only the zPIV supply was off (due to serial duplication off-chain on main net)
     if (Params().NetworkID() == CBaseChainParams::MAIN && pindex->nHeight == consensus.height_last_ZC_WrappedSerials + 1
             && pindex->GetZerocoinSupply() < consensus.ZC_WrappedSerialsSupply + GetWrapppedSerialInflationAmount()) {
-        for (auto denom : libzerocoin::zerocoinDenomList) {
+        for (const auto& denom : libzerocoin::zerocoinDenomList)
             pindex->mapZerocoinSupply.at(denom) += GetWrapppedSerialInflation(denom);
-        }
     }
+
+    for (const auto& denom : libzerocoin::zerocoinDenomList)
+        LogPrint("zero", "%s coins for denomination %d pubcoin %s\n", __func__, denom, pindex->mapZerocoinSupply.at(denom));
+
     return true;
 }
 

src/consensus/zerocoin_verify.h
@@ -21,7 +21,7 @@ bool CheckPublicCoinSpendVersion(int version);
 bool ContextualCheckZerocoinSpend(const CTransaction& tx, const libzerocoin::CoinSpend* spend, int nHeight, const uint256& hashBlock);
 bool ContextualCheckZerocoinSpendNoSerialCheck(const CTransaction& tx, const libzerocoin::CoinSpend* spend, int nHeight, const uint256& hashBlock);
 void AddWrappedSerialsInflation();
-bool RecalculateVITSupply(int nHeightStart);
+bool RecalculateVITSupply(int nHeightStart, bool fSkipZpiv = true);
 bool UpdateZVITAESupply(const CBlock& block, CBlockIndex* pindex, bool fJustCheck);
 CAmount GetInvalidUTXOValue();
 

src/init.cpp
@@ -1571,9 +1571,8 @@ bool AppInit2()
                 // Recalculate money supply for blocks that are impacted by accounting issue after zerocoin activation
                 if (GetBoolArg("-reindexmoneysupply", false) || reindexZerocoin) {
                     // Recalculate from the zerocoin activation or from scratch.
-                    RecalculateVITSupply(reindexZerocoin ? consensus.height_start_ZC : 1);
+                    RecalculateVITSupply((reindexZerocoin ? consensus.height_start_ZC : 1), false);
                 }
-
 
                 if (!fReindex) {
                     uiInterface.InitMessage(_("Verifying blocks..."));