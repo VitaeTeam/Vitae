26d6fe550910837ed4c972d3a348ab2317222dd1
tohsnoom <moonshot@phore.io>
2020-12-13 18:46:00 -0600

    Fixed OSX compile error in accumulators.cpp

--- src/accumulators.cpp
+++ src/accumulators.cpp
@@ -20,7 +20,7 @@ std::list<uint256> listAccCheckpointsNoDB;
 uint32_t ParseChecksum(uint256 nChecksum, CoinDenomination denomination)
 {
     //shift to the beginning bit of this denomination and trim any remaining bits by returning 32 bits only
-    int pos = distance(zerocoinDenomList.begin(), find(zerocoinDenomList.begin(), zerocoinDenomList.end(), denomination));
+    int pos = std::distance(zerocoinDenomList.begin(), find(zerocoinDenomList.begin(), zerocoinDenomList.end(), denomination));
     nChecksum = nChecksum >> (32*((zerocoinDenomList.size() - 1) - pos));
     return nChecksum.Get32();
 }
