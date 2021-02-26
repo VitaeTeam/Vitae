248ffe7b17633f22d3beaf004e03aac4d0ff61d2
tohsnoom <moonshot@phore.io>
2021-02-25 23:24:40 -0600

    Changed minimum protocol before enforcement to 71010

--- src/version.h
+++ src/version.h
@@ -20,7 +20,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71024;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71010;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71025;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
