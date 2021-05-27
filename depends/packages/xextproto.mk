a4bd565d53df9f43e8cf9322f0f38c2ab7078f64
wqking <wqking@outlook.com>
2021-05-27 21:00:53 +0800

    Update depends with Vitae v4.6.4

--- depends/packages/xextproto.mk
+++ depends/packages/xextproto.mk
@@ -1,11 +1,12 @@
 package=xextproto
 $(package)_version=7.3.0
-$(package)_download_path=https://xorg.freedesktop.org/releases/individual/proto
+$(package)_download_path=http://xorg.freedesktop.org/releases/individual/proto
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0
+$(package)_patches=fix_aarch64_build.patch
 
 define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+  patch -p1 < $($(package)_patch_dir)/fix_aarch64_build.patch
 endef
 
 define $(package)_set_vars
